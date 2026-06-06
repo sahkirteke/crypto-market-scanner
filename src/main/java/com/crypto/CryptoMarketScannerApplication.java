28750da757587cfbd9137395918f4a77ed1ca515
SHK <husamettin.kirteke@yepz.com>
2026-06-06 22:42:13 +0300

    first commit

---
+++ src/main/java/com/crypto/CryptoMarketScannerApplication.java
@@ -0,0 +1,13 @@
+package com.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMarketScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoMarketScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/CryptoMarketScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMarketScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
